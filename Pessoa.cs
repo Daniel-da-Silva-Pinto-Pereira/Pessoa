af55b0c8f13b6a8fab67fb259d04adaf41ac2054
Daniel da Silva <danieldasilva@ucl.br>
2020-09-12 20:02:52 +0000

    Initial commit

---
+++ Pessoa.cs
@@ -0,0 +1,46 @@
+using System;
+/*Crie uma classe que modele uma pessoa:
+
+Atributos: nome, idade, peso e altura
+Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.*/
+
+class  pessoa {
+	
+	public string nome;
+	public int idade;
+	public double peso, altura;
+
+	public void envelhecer(int ano){
+
+    if (idade < 21) {
+      altura += ano*0.5;
+    }
+  }
+  public int envelhecer(){
+    return idade;
+  }
+  
+  public double engordar(double ganho_de_peso){
+    peso += ganho_de_peso; 
+  }
+
+  public double retorna_engordar(){
+    return peso;
+  }
+
+  public double emagrecer(double peso_perdido){
+    peso -= peso_perdido; 
+  } 
+
+  public double retornar_emagrecer(){
+    return peso;
+  }
+
+  public double crescer(double ganho_de_altura){
+    altura += ganho_de_altura;
+  }
+
+  public double retornar_crescer(){
+    return altura;
+  }
+}
